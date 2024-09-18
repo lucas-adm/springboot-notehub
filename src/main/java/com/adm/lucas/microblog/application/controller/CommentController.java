e7a87d871fd6d0f7ccdb2ef77ed756090822af7c
Lucas Aguiar <vintesetelucas@gmail.com>
2024-09-18 15:55:56 -0300

    📚 docs: add documentation for Comment controller

--- src/main/java/com/adm/lucas/microblog/application/controller/CommentController.java
+++ src/main/java/com/adm/lucas/microblog/application/controller/CommentController.java
@@ -7,6 +7,13 @@ import com.adm.lucas.microblog.application.dto.response.page.PageRES;
 import com.adm.lucas.microblog.domain.comment.Comment;
 import com.adm.lucas.microblog.domain.comment.CommentService;
 import com.auth0.jwt.JWT;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +30,8 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/notes")
+@SecurityRequirement(name = "bearer-key")
+@Tag(name = "Comment Controller", description = "Endpoints for managing comments")
 @RequiredArgsConstructor
 public class CommentController {
 
@@ -33,10 +42,18 @@ public class CommentController {
         return UUID.fromString(idFromToken);
     }
 
+    @Operation(summary = "Register a new comment", description = "Creates a new comment.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Comment registered successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input data.", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "Invalid token.", content = @Content(examples = {})),
+            @ApiResponse(responseCode = "404", description = "Note note found.", content = @Content(examples = {})),
+            @ApiResponse(responseCode = "500", description = "Internal server error.", content = @Content(examples = {}))
+    })
     @PostMapping("/{id}/new-comment")
     @Transactional
     public ResponseEntity<CreateCommentRES> createComment(
-            @RequestHeader("Authorization") String accessToken,
+            @Parameter(hidden = true) @RequestHeader("Authorization") String accessToken,
             @PathVariable("id") UUID idFromPath,
             @Valid @RequestBody CreateCommentREQ dto
     ) {
@@ -45,10 +62,18 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new CreateCommentRES(comment));
     }
 
+    @Operation(summary = "Change comment text", description = "Updates comment text field.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Comment text updated successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input data.", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "Invalid token.", content = @Content(examples = {})),
+            @ApiResponse(responseCode = "404", description = "Comment note found."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.", content = @Content(examples = {}))
+    })
     @PatchMapping("/comment/{id}/edit-comment")
     @Transactional
     public ResponseEntity<Void> editComment(
-            @RequestHeader("Authorization") String accessToken,
+            @Parameter(hidden = true) @RequestHeader("Authorization") String accessToken,
             @PathVariable("id") UUID idFromPath,
             @Valid @RequestBody CreateCommentREQ dto
     ) {
@@ -57,10 +82,17 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
+    @Operation(summary = "Delete comment", description = "Removes a comment permanently.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Comment deleted successfully."),
+            @ApiResponse(responseCode = "403", description = "Invalid token.", content = @Content(examples = {})),
+            @ApiResponse(responseCode = "404", description = "Comment not found.", content = @Content(examples = {})),
+            @ApiResponse(responseCode = "500", description = "Internal server error.", content = @Content(examples = {}))
+    })
     @DeleteMapping("/comment/{id}")
     @Transactional
     public ResponseEntity<Void> deleteComment(
-            @RequestHeader("Authorization") String accessToken,
+            @Parameter(hidden = true) @RequestHeader("Authorization") String accessToken,
             @PathVariable("id") UUID idFromPath
     ) {
         UUID idFromToken = getSubject(accessToken);
@@ -68,6 +100,12 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Get comments", description = "Retrieves a comments page.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Comments retrieved successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid pageable criteria.", content = @Content(examples = {})),
+            @ApiResponse(responseCode = "500", description = "Internal server error.", content = @Content(examples = {}))
+    })
     @GetMapping("/{id}/comments")
     public ResponseEntity<PageRES<DetailCommentRES>> getComments(
             @ParameterObject @PageableDefault(page = 0, size = 10, sort = {"createdAt"}, direction = Sort.Direction.DESC) Pageable pageable,
